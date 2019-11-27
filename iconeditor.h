d3395d761b9c3406e9eb13aa90cc0d6f330d5a06
bovkun <alexandr05@list.ru>
2019-11-27 12:57:25 +0200

    Init repo

---
+++ iconeditor.h
@@ -0,0 +1,14 @@
+#ifndef ICONEDITOR_H
+#define ICONEDITOR_H
+
+#include <QWidget>
+
+class IconEditor : public QWidget
+{
+	Q_OBJECT
+
+public:
+	IconEditor(QWidget *parent = nullptr);
+	~IconEditor();
+};
+#endif // ICONEDITOR_H
